62c18bf286875fd7bbe714f34110b36171d10878
codingalrish <codingalrish@gmail.com>
2026-02-18 00:58:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, CI/CD World from Python!"
+    return "Hello, 4CI/CD World from Python!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
